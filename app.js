fc7319fe3c468b30e70cfd490a178422677ccf4f
Alessandro Menezes <alessandroadm@live.com>
2020-06-11 05:07:07 -0300

    configuração de produção

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cookieSession({
   name: 'session',
-  keys: [process.env.COOKIE_KEY],
+  keys: ['1ODkwIiwibmGtZSI4IkpvaG3'],
   resave: false,
   saveUninitialized: false,
   maxAge: 60 * 60 * 24 * 1000
@@ -46,7 +46,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'production' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
